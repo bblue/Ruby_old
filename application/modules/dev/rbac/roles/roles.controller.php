1d227ebb71d82ddd9e6e0dd9945a19dfcaff9c7a
Aleksander <aleksander.lanes@gmail.com>
2014-04-13 20:24:01 +0200

    Namespace corrections

--- application/modules/dev/rbac/roles/roles.controller.php
+++ application/modules/dev/rbac/roles/roles.controller.php
@@ -3,30 +3,22 @@ namespace Modules\Dev\Rbac;
 
 use Modules\Dev\RbacController;
 
-use App\AbstractController;
-
 final class RolesController extends RbacController
-{	
+{
 	protected function executeAddrbacroles()
 	{
-		if($this->rbac->Check('DEV_AREA', $this->visitor->user_id)) {
-			if($this->request->submit) {
-				$dev = $this->serviceFactory->build('dev');
+		if($this->request->submit) {
+			$dev = $this->serviceFactory->build('dev');
 
-				$array[] = array(
-					'title'			=> 'Guest',
-					'description'	=> 'This is the guest role111',
-					'parent_id'		=> null
-				);
+			$array[] = array(
+				'title'			=> 'Guest',
+				'description'	=> 'This is the guest role111',
+				'parent_id'		=> null
+			);
 
-				if($dev->addRbacRoles($array)) {
-					return $this->load('successfulCommand');
-				}
-			} else {
-				return true;
+			if($dev->addRbacRoles($array)) {
+				return $this->load('successfulCommand');
 			}
-		} else {
-	    	return $this->load('set403error');
 		}
 	}
 }

0a69b2247b7a4cc9b0f049034a06a6c117179769
Aleksander <aleksander.lanes@gmail.com>
2014-04-14 18:14:34 +0200

    Removed RBAC from abstract controller

--- application/app/abstractcontroller.php
+++ application/app/abstractcontroller.php
@@ -12,7 +12,7 @@ abstract class AbstractController
 	protected $request;
 	protected $eventHandler;
 	private $view;
-	
+
 	protected $rbac;
 	protected $visitor;
 	protected $log;
@@ -22,20 +22,19 @@ abstract class AbstractController
 		$this->serviceFactory = $serviceFactory;
 		$this->request = $request;
 	}
-	
+
 	protected function load($sCommand)
 	{
 		if(PRINT_CONTROLLER_COMMAND === true) {
     	    $classname = strtolower(get_called_class());
-    	
-    	    if (preg_match('@\\\\([\w]+)$@', $classname, $matches)) {
+
+    	    if(preg_match('@\\\\([\w]+)$@', $classname, $matches)) {
     	        $classname = $matches[1];
-    	    }	
+    	    }
 			echo '<pre>Performing command on controller: <i>$' . $classname . '->' . $sCommand . '();</i></pre>';
 		}
-		
+
 		$sCommand = ucfirst(strtolower($sCommand));
-		
 		$mutator = 'execute' . $sCommand;
 
 		if (!method_exists($this, $mutator) || !is_callable(array($this, $mutator))) {
@@ -43,28 +42,24 @@ abstract class AbstractController
 		}
 
 		// Inform the view of what command we are performing
-		$this->view->setCommand($sCommand);
+		$this->view->setCommand($sCommand); //@todo: Check if this is really required
 
 		return $this->$mutator();
 	}
 
 	public function execute($sCommand, View $view)
 	{
-		// Load the RBAC at the last minute
-		require_once ROOT_PATH . DIRECTORY_SEPARATOR . 'lib'. DIRECTORY_SEPARATOR . 'PhpRbac'. DIRECTORY_SEPARATOR . 'autoload.php'; //@todo: Create a service from this
-		$this->rbac = new \PhpRbac\Rbac();
-
 		// Load the logging mechanism
 		$this->log = $this->serviceFactory->build('logging', true);
-		
+
 		// Load the event handler
 		$this->eventHandler = $this->serviceFactory->build('eventHandler', true);
-		
+
 		// Preload the visitor
 		$this->visitor = $this->serviceFactory->build('recognition', true)->getCurrentVisitor();
-		
+
 		$this->view = $view;
-		
+
 		return $this->load($sCommand);
 	}
 

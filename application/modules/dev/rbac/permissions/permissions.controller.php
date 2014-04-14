d86ac057776fa5a0ea3d7b6f6f008b915fd8fc49
Aleksander <aleksander.lanes@gmail.com>
2014-04-14 18:17:44 +0200

    Namespace correction

--- application/modules/dev/rbac/permissions/permissions.controller.php
+++ application/modules/dev/rbac/permissions/permissions.controller.php
@@ -1,29 +1,29 @@
 <?php
-namespace Controllers\Dev\Rbac;
+namespace Modules\Dev\Rbac;
 
-use App\AbstractController;
+use Modules\Dev\RbacController;
 
-final class Permissions extends Rbac
+final class PermissionsController extends RbacController
 {
 	protected function executeAddrbacpermissions()
 	{
 		if($this->rbac->Check('DEV_AREA', $this->visitor->user_id)) {
 			if($this->request->submit) {
 				$dev = $this->serviceFactory->build('dev');
-				
+
 				$array[] = array(
 					'title'			=> 'Permission',
 					'description'	=> 'This is a permission',
 					'parent_id'		=> $this->request->parent_id
 				);
-				
+
 				if($dev->addRbacPermissions($array)) {
 					return $this->load('successfulCommand');
 				}
 			} else {
 				// Standard view for this command
 				return true;
-			}			
+			}
 		} else {
 	    	return $this->load('set403error');
 		}
--- application/modules/dev/rbac/permissions/permissions.view.php
+++ application/modules/dev/rbac/permissions/permissions.view.php
@@ -1,37 +1,36 @@
 <?php
 namespace Modules\Dev\Rbac;
 
-use App\AbstractView,
-	App\Template;
+use Modules\Dev\RbacView;
 
 final class PermissionsView extends RbacView
 {
     protected function executeIndexaction()
     {
         $sTemplateFile = 'rbac/add_permission';
-    
+
         /** Load rbac role tree */
         $this->presentationObjectFactory
         ->build('rbac_role_tree', true)
         ->assignData();
-        	
+
         /** Load rbac permission tree */
         $this->presentationObjectFactory
         ->build('rbac_permission_tree', true)
         ->assignData();
-        	
+
         /** Load required scripts */
         $this->presentationObjectFactory
         ->build('scripttags', true)
         ->assignData($sTemplateFile);
-    
+
         $this->display('custom/header.htm');
         $this->display('custom/sidebar.htm');
         $this->display('custom/rightbar.htm');
         $this->display('custom/' . $sTemplateFile . '.htm');
         $this->display('custom/footer.htm');
     }
-    	
+
 	protected function executeAddrbacpermissions()
 	{
 

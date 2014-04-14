6548e7993682d89c51612445ed63f2f15ff5b0cc
Aleksander <aleksander.lanes@gmail.com>
2014-04-14 17:20:59 +0200

    deleted whitespace

--- application/modules/error/error.view.php
+++ application/modules/error/error.view.php
@@ -12,58 +12,56 @@ final class ErrorView extends AbstractView
 			->build('errormessage', true)
 			->setTemplatePrefix('http_error')
 			->assignData(404, 'Page Not Found', 'The page you requested could not be found');
-		
+
 		$sTemplateFile = 'error/extras-404';
-		
+
 		/** Load required scripts */
 		$this->presentationObjectFactory
 		->build('scripttags', true)
 		->assignData($sTemplateFile);
-			
+
 		if($this->serviceFactory->build('recognition', true)->getCurrentVisitor()->isLoggedIn() || FORCED_LOGIN === false) {
-			
-					
+
+
 			$this->display('custom/header.htm');
 			$this->display('custom/sidebar.htm');
 			$this->display('custom/rightbar.htm');
 			$this->display('custom/' . $sTemplateFile . '.htm');
-			$this->display('custom/footer.htm');	
+			$this->display('custom/footer.htm');
 		} else {
 			$this->display('custom/error/full-page-error.htm');
 		}
-		
+
 		return true;
 	}
-	
+
 	public function executeSet500error()
-	{		
+	{
 		http_response_code(500);
-		
+
 		$this->presentationObjectFactory
 			->build('errormessage', true)
 			->setTemplatePrefix('http_error')
 			->assignData(500, 'Internal Server Error', 'An internal server error occured');
 
 		$sTemplateFile = 'error/extras-500';
-		
+
 		/** Load required scripts */
 		$this->presentationObjectFactory
 		->build('scripttags', true)
 		->assignData($sTemplateFile);
-					
+
 		if($this->serviceFactory->build('recognition', true)->getCurrentVisitor()->isLoggedIn() || FORCED_LOGIN === false) {
-			
-					
 			$this->display('custom/header.htm');
 			$this->display('custom/sidebar.htm');
 			$this->display('custom/rightbar.htm');
 			$this->display('custom/' . $sTemplateFile . '.htm');
-			$this->display('custom/footer.htm');	
+			$this->display('custom/footer.htm');
 		} else {
 			$this->display('custom/error/full-page-error.htm');
 		}
-		
+
 		return true;
-	}	
-	
-} 
+	}
+
+}

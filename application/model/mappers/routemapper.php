7f53a0dd73b78beccca7f7c8c0b8a3f76a16d700
Aleksander <aleksander.lanes@gmail.com>
2014-04-14 18:16:28 +0200

    Removed un-used usergroup enity (and more)

--- application/model/mappers/routemapper.php
+++ application/model/mappers/routemapper.php
@@ -1,6 +1,6 @@
 <?php
 namespace Model\Mappers;
-use 
+use
 	Model\Domain\Route\Route,
 	App\DatabaseDataMapper,
 	App\AbstractEntity,
@@ -9,23 +9,22 @@ use
 final class RouteMapper extends DatabaseDataMapper
 {
 	protected $_entityClass = 'Route';
-	
+
 	protected $_acceptedFields = array(
 		'id'			=> 'routes.id',
 		'url'			=> 'routes.url',
 		'sResourceName'	=> 'routes.sResourceName',
-		/*'bIsEnabled'	=> 'areas.bIsEnabled',*/
+		'bIsEnabled'	=> 'areas.bIsEnabled',
 		'sCommand'		=> 'routes.sCommand',
-		/*'a_id'			=> 'areas.a_id',
+		'a_id'			=> 'areas.a_id',
 		'bCanBypassForcedLogin' => 'commands.bCanBypassForcedLogin',
-		*/
 	);
 	protected $_cascadeFields = array(
-		/*'routes.sResourceName = areas.name',
+		'routes.sResourceName = areas.name',
 		'commands.sName = routes.sCommand',
-		'commands.a_id = areas.a_id'*/
+		'commands.a_id = areas.a_id'
 	);
-    
+
     public function fetch(AbstractEntity $route)
     {
     	// Check if ID has been set
@@ -39,7 +38,7 @@ final class RouteMapper extends DatabaseDataMapper
     	        )
     	    );
 	    	$this->find($aCriterias, $route);
-	    	
+
 	    	if(!isset($route->id)) {
 	    	    // Try route via url
 	    	    $aCriterias['url'] = array(
@@ -54,21 +53,11 @@ final class RouteMapper extends DatabaseDataMapper
 
     	return $route;
     }
-    
+
     protected function setEntitySpecificData(AbstractEntity $route)
     {
 		// Find the users that have access to the area in this route
 		//$route->users = new CollectionProxy($this->_dataMapperFactory->build('user'), array('a_id' => array(array('operator' => '=', 'value' => $route->a_id))));
-		
-    	// Find the usergroups that have access to the area in this route
-		/*$route->usergroups = new CollectionProxy(
-			$this->_dataMapperFactory->build('usergroup'),
-			array('active' => array(array('operator' => '=','value' => 1))),
-			array(
-				'cascade_a_usergroups.a_id' => array(array('operator' => '=','value' => $route->a_id)),
-				'id' 						=> array(array('operator' => '=','value' => 'cascade_a_usergroups.ug_id', 'tablevalue' => true))
-			)
-		);
-		*/
+
     }
 }

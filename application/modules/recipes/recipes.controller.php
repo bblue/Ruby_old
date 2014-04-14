53dcf155f3c5cd623b97df2ffe3be2ddddc745f4
Aleksander <aleksander.lanes@gmail.com>
2014-04-14 17:22:21 +0200

    Enabled eventHandler in recipes.controller

--- application/modules/recipes/recipes.controller.php
+++ application/modules/recipes/recipes.controller.php
@@ -9,29 +9,29 @@ final class RecipesController extends AbstractController
 	{
 		return $this->load('managemyrecipes');
 	}
-	
+
 	public function executeManagemyrecipes()
 	{
 		return true;
 	}
-	
+
 	public function executeView()
 	{
 	    return true;
 	}
-	
+
 	public function executeAdd()
 	{
-	    //$event = $this->eventHandler->buildEvent(array('user' => $user));
-	    $this->eventHandler->dispatch('recipes.add' /*$event*/);
+	    $event = $this->eventHandler->buildEvent(array('user' => $user));
+	    $this->eventHandler->dispatch('recipes.add', $event);
 		return true;
 	}
-	
+
 	public function executeValidate()
 	{
 		return true;
 	}
-	
+
 	protected function executeGetcategories()
 	{
 		return true;

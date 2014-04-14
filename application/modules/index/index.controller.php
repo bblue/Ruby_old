23b3f9724a694a0ec84c51254dbcc097896afa86
Aleksander <aleksander.lanes@gmail.com>
2014-04-14 17:21:56 +0200

    Removed RBAC from controllers

--- application/modules/index/index.controller.php
+++ application/modules/index/index.controller.php
@@ -7,11 +7,7 @@ final class IndexController extends AbstractController
 {
 	protected function executeIndexaction()
 	{
-		if($this->rbac->Check('VIEW_DASHBOARD', $this->visitor->user_id)) {
-			return true;
-		} else {
-			return $this->load('set403error');
-		}
+
 	}
 
 }

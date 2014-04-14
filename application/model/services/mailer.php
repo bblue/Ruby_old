a60dbe430c918fc7e606974002f312c2f8a2adaa
Aleksander <aleksander.lanes@gmail.com>
2014-04-14 18:15:19 +0200

    Replaced hard coded settings with constants

--- application/model/services/mailer.php
+++ application/model/services/mailer.php
@@ -5,20 +5,29 @@ use App\ServiceAbstract;
 
 final class Mailer extends ServiceAbstract
 {
-    public $mail; 
-    
+    public $mail;
+
+    private $FROM_ADDRESS 		= MAIL_FROM_ADDRESS;
+    private $FROM_NAME			= MAIL_FROM_NAME;
+    private $SERVER_USERNAME	= MAIL_SERVER_USERNAME;
+    private $SERVER_PASSWORD	= MAIL_SERVER_PASSWORD;
+    private $SERVER_ADDR_SSL	= MAIL_SERVER_ADDR_SSL;
+    private $SERVER_PORT_SSL	= MAIL_SERVER_PORT_SSL;
+    private $SERVER_ADDR		= MAIL_SERVER_ADDR;
+    private $SERVER_PORT		= MAIL_SERVER_PORT;
+
     public function __construct()
     {
         require(ROOT_PATH . '/lib/PHPMailer/phpmailer.php');
         $this->mail = new \PhpMailer();
 
         $this->loadDefaultConfiguration();
-        
+
         $this->configureNonSSL();
-        
+
         return $this->mail;
     }
-    
+
     public function send()
     {
         if(!$this->mail->send()) {
@@ -27,33 +36,33 @@ final class Mailer extends ServiceAbstract
             return true;
         }
     }
-    
+
     private function loadDefaultConfiguration()
     {
         $this->mail->isSMTP();
         $this->mail->SMTPAuth = true;
         // $this->mail->SMTPDebug = 2; //Show detailed messages
-        
-        $this->mail->Username = 'webmaster@intensjon.no';
-        $this->mail->Password = '1afroide2';
-        
+
+        $this->mail->Username = $this->SERVER_USERNAME;
+        $this->mail->Password = $this->SERVER_PASSWORD;
+
         $this->CharSet = 'UTF-8';
         $this->mail->WordWrap = 900;
-        
-        $this->mail->From = 'webmaster@intensjon.no';
-        $this->mail->FromName = 'beta.intensjon.no';
+
+        $this->mail->From = $this->FROM_ADDRESS;
+        $this->mail->FromName = $this->FROM_NAME;
     }
-    
+
     private function configureSSL()
     {
-        $this->mail->Host = 'v1.golarge.net';
-        $this->mail->Port = 465; // SMTP port
+        $this->mail->Host = $this->SERVER_ADDR_SSL;
+        $this->mail->Port = $this->SERVER_PORT_SSL; // SMTP port
         $this->mail->SMTPSecure = 'ssl'; // 'ssl', 'tls' or ''
     }
-    
+
     private function configureNonSSL()
     {
-        $this->mail->Host = 'mail.intensjon.no';
-        $this->mail->Port = 26; // SMTP port
+        $this->mail->Host = $this->SERVER_ADDR;
+        $this->mail->Port = $this->SERVER_PORT; // SMTP port
     }
 }

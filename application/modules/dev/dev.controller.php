5443e0841ef29efe31efe449438ae038573fd7f8
Aleksander <aleksander.lanes@gmail.com>
2014-04-14 18:14:54 +0200

    Removed RBAC from dev controller

--- application/modules/dev/dev.controller.php
+++ application/modules/dev/dev.controller.php
@@ -4,13 +4,9 @@ namespace Modules;
 use App\AbstractController;
 
 final class DevController extends AbstractController
-{	
+{
 	protected function executeIndexaction()
 	{
-		if($this->rbac->Check('VIEW_DEV_AREA', $this->visitor->user_id)) {
-			return true;
-		} else {
-			return $this->load('set403error');
-		}
+
 	}
 }
